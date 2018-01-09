0dc8eacc17ce385083c690b6510599149f708610
Mark Chen <markchen555@gmail.com>
2018-01-09 01:54:07 -0800

    Doc: Created video_list and imported into index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1nO27DPO_5Ss';
-
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
 
 class App extends Component {
   constructor(props) {
@@ -17,12 +14,18 @@ class App extends Component {
     this.state = {
       videos: []
     };
+
+    YTSearch({key: API_KEY, term: 'Game'}, (videos) => {
+      this.setState({ videos }); // ES6 way if key and value are the same, this.setState({ videos: videos})
+      console.log(this.state.videos)
+    });
   }
 
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={ this.state.videos } />
       </div>
     );
   }
