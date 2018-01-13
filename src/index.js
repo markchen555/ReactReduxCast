410aae59fa061fa3ad27d754217825b28336dafc
Mark Chen <markchen555@gmail.com>
2018-01-13 00:12:50 -0800

    Doc: implement lodash debounce into videoSearch

--- src/index.js
+++ src/index.js
@@ -31,13 +31,18 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300); // throttle user input to trigger videoSearch every 300 m/s.
     return (
-      <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
-        <VideoDetail video={ this.state.selectedVideo } />
-        <VideoList 
-        onVideoSelect = { selectedVideo => this.setState({ selectedVideo })}
-        videos={ this.state.videos } />
+      <div className="container">
+        <div>
+          <SearchBar onSearchTermChange={ videoSearch } />
+        </div>
+        <div className="row">
+          <VideoDetail video={ this.state.selectedVideo } />
+          <VideoList 
+          onVideoSelect = { selectedVideo => this.setState({ selectedVideo })}
+          videos={ this.state.videos } />
+        </div>
       </div>
     );
   }
